f63563ba410d95fddbf843847596e8da592083c2
Melody4589 <manoftheyear4589@gmail.com>
2024-05-02 05:53:19 -0700

    completed app

---
+++ app/page.jsx
@@ -0,0 +1,33 @@
+"use client"
+import { useState } from "react";
+import "./style.css";
+export default function Page() {
+    var [money, updatebrowser] = useState(300);
+
+
+    function addMoney() {
+        money += 400;
+        updatebrowser(money)
+
+    }
+    function subMoney() {
+        money -= 300;
+        updatebrowser(money)
+
+    }
+
+
+    return (
+        <main>
+            <h1>My Money App</h1>
+            <p>My Savings for the year so far</p>
+            <h4>NGN {money}</h4>
+            <button className="btn-minus" onClick={subMoney}>
+                <i className="fa fa-minus"></i>
+            </button>
+            <button className="btn-plus" onClick={addMoney}>
+                <i className="fa fa-plus"></i>
+            </button>
+        </main>
+    )
+}
